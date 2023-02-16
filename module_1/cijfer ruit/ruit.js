ce7a2782a2b896b97717ca3406e7f6b05d64cc8a
Jelle Fiere <99071834@mydavinci.nl>
2023-02-16 10:18:36 +0100

    ruit

--- module_1/cijfer ruit/ruit.js
+++ module_1/cijfer ruit/ruit.js
@@ -1,7 +1,15 @@
 let alert = prompt("geef een getal: ")
 
-for(let i = 0; i = alert;i++) {
-    text+= i ;
+const listnum = []
+
+
+
+
+for(let i = 0; i< alert; i++) {
+    for(let z = 0; z < i+1; z++){
+        document.write(z+ 1 + "-" )
+    }
+    document.write("<br>")
 }
-document.getElementById("cijfer").innerHTML = text;
+
 
