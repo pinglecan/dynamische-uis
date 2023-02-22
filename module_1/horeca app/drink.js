354b934ce9965c1ff644080b395a85bea0441c9f
Jelle Fiere <99071834@mydavinci.nl>
2023-02-22 09:24:36 +0100

    real

---
+++ module_1/horeca app/drink.js
@@ -0,0 +1,11 @@
+while (bestellen != 'stop') {
+
+let bestellen = promt("Wat wilt u bestellen")
+
+    if(bestellen == "fris" || "bier" || "wijn"){
+
+        let hoeveel = promt("Hoeveel" + {bestellen} + " wilt u bestellen")
+
+    }
+
+}
