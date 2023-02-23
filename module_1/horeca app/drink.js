41f6fafc5eaf9a9fcb9baa74b085718ac0cc3239
Jelle Fiere <99071834@mydavinci.nl>
2023-02-23 09:45:42 +0100

    Update drink.js

--- module_1/horeca app/drink.js
+++ module_1/horeca app/drink.js
@@ -1,15 +1,41 @@
-bestellen = "real"
+let bestellen = "real"
+let hoeveel_bier =0
+let hoeveel_fris = 0
+let hoeveel_wijn = 0
+let hoeveel = 0 
+
+
 
 while (bestellen != 'stop') {
 
-    bestellen = prompt ("Wat wilt u bestellen")
+    bestellen = prompt ("Wat wilt u bestellen").toLowerCase()
 
-    if (bestellen == "fris" || "bier" || "wijn"){
+    if (bestellen == "fris" ||bestellen == "bier" || bestellen == "wijn"){
 
-        let hoeveel = prompt ("Hoeveel wilt u bestellen")
-    
-    } else if{
-        
+        hoeveel = parseInt(prompt ("Hoeveel wilt u bestellen"))
+        if (bestellen == "fris"){
+            hoeveel_fris = hoeveel_fris + hoeveel
+            document.write("fris: x" + hoeveel_fris)
+            document.write("<br>")
+        }else if(bestellen == "bier"){
+            hoeveel_bier = hoeveel_bier + hoeveel
+            document.write("bier: x" + hoeveel_bier)
+            document.write("<br>")
+        }else if(bestellen == "wijn"){
+            hoeveel_wijn = hoeveel_wijn + hoeveel
+            document.write("wijn: x" + hoeveel_wijn)
+            document.write("<br>")
+        }
+
+
+
+
+
+    }else if(bestellen == 'stop'){
+        alert('bedankt voor het bestellen')
+    }else{
+        alert ("Die bestaat niet. kies uit Fris, Bier of Wijn.")
     }
-
 }
+
+console.log(hoeveel_bier)
