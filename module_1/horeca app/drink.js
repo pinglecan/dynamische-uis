81db7434cd4a20956ad03f81ba63c039fa26c31d
Jelle Fiere <99071834@mydavinci.nl>
2023-03-08 10:15:57 +0100

    horeca app

--- module_1/horeca app/drink.js
+++ module_1/horeca app/drink.js
@@ -4,37 +4,24 @@ let hoeveel_fris = 0
 let hoeveel_wijn = 0
 let hoeveel = 0 
 
-const drinken = []
+const drinken = {wijn:0, bier:0, fris:0}
 
 while (bestellen != 'stop') {
 
     bestellen = prompt ("Wat wilt u bestellen").toLowerCase()
 
     if (bestellen == "fris" || bestellen == "bier" || bestellen == "wijn"){
-
         hoeveel = parseInt(prompt ("Hoeveel wilt u bestellen"))
-        if (bestellen == "fris"){
-            hoeveel_fris = hoeveel_fris += Number(hoeveel)
-            drinken.push("fris: x" + hoeveel_fris)
-        }else if(bestellen == "bier"){
-            hoeveel_bier = hoeveel_bier += Number(hoeveel)
-            drinken.push("bier: x" + hoeveel_bier)
-        }else if(bestellen == "wijn"){
-            hoeveel_wijn = hoeveel_wijn += Number(hoeveel)
-            drinken.push("wijn: x" + hoeveel_wijn)
-        }
-
-
-
-
-
+        drinken[bestellen] += hoeveel
     }else if(bestellen == 'stop'){
-        alert('bedankt voor het bestellen')
+        alert('beda nkt voor het bestellen')
     }else{
         alert ("Die bestaat niet. kies uit Fris, Bier of Wijn.")
     }
 }
 
-console.log(hoeveel_bier)
+console.dir(drinken)
 
-document.write(drinken)
+const itemsFormatted = JSON.stringify(drinken, null, " ");
+console.dir(itemsFormatted)
+document.getElementById("myDiv").innerHTML = itemsFormatted;
