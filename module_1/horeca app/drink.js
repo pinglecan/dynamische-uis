511d838ea7d9216a74fe7b243b0172288bf53786
Jelle Fiere <99071834@mydavinci.nl>
2023-03-08 09:09:21 +0100

    Update drink.js

--- module_1/horeca app/drink.js
+++ module_1/horeca app/drink.js
@@ -4,27 +4,24 @@ let hoeveel_fris = 0
 let hoeveel_wijn = 0
 let hoeveel = 0 
 
-
+const drinken = []
 
 while (bestellen != 'stop') {
 
     bestellen = prompt ("Wat wilt u bestellen").toLowerCase()
 
-    if (bestellen == "fris" ||bestellen == "bier" || bestellen == "wijn"){
+    if (bestellen == "fris" || bestellen == "bier" || bestellen == "wijn"){
 
         hoeveel = parseInt(prompt ("Hoeveel wilt u bestellen"))
         if (bestellen == "fris"){
-            hoeveel_fris = hoeveel_fris + hoeveel
-            document.write("fris: x" + hoeveel_fris)
-            document.write("<br>")
+            hoeveel_fris = hoeveel_fris += Number(hoeveel)
+            drinken.push("fris: x" + hoeveel_fris)
         }else if(bestellen == "bier"){
-            hoeveel_bier = hoeveel_bier + hoeveel
-            document.write("bier: x" + hoeveel_bier)
-            document.write("<br>")
+            hoeveel_bier = hoeveel_bier += Number(hoeveel)
+            drinken.push("bier: x" + hoeveel_bier)
         }else if(bestellen == "wijn"){
-            hoeveel_wijn = hoeveel_wijn + hoeveel
-            document.write("wijn: x" + hoeveel_wijn)
-            document.write("<br>")
+            hoeveel_wijn = hoeveel_wijn += Number(hoeveel)
+            drinken.push("wijn: x" + hoeveel_wijn)
         }
 
 
@@ -39,3 +36,5 @@ while (bestellen != 'stop') {
 }
 
 console.log(hoeveel_bier)
+
+document.write(drinken)
