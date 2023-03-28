f4b5f735c999364677594be84cb7c3c2bd1b28cd
Jelle Fiere <99071834@mydavinci.nl>
2023-03-28 10:39:21 +0200

    .

--- module_2/buttons/buttons.js
+++ module_2/buttons/buttons.js
@@ -5,6 +5,10 @@ function changeImage(nummers){
     plaatje = document.getElementById('plaatje')
     plaatje.src = "images/"+nummers+".jpg"
     button.innerHTML = parseInt(button.innerHTML) + 1
-    
-    button.disabled = true  
+    button.disabled = true;
+    for(let i = 1; i !=4; i++){
+        console.log(i)
+        document.getElementById('button' + i ).disabled = false;
+    }
+    button.disabled = true;
 }
