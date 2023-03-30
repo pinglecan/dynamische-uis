7a547b871b41ad9cef515946181b6ae16ec09dc1
Jelle Fiere <99071834@mydavinci.nl>
2023-03-30 10:03:43 +0200

    knop

---
+++ challange/waar is mijn knop/knop.js
@@ -0,0 +1,18 @@
+jouwnaam = prompt('wat is je naam?')
+let teller = 0
+
+
+var button = document.createElement("button");
+button.innerHTML = "Click me";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+    document.getElementById('welkom').innerHTML = 'welkom ' + jouwnaam
+    teller += 1
+    if(teller == 2){
+        console.log('ball aidss')
+        button.remove()
+    }
+});     
