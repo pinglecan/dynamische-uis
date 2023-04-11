9f9ee88423ee23406b3da7421df2790f32e5b939
Jelle Fiere <99071834@mydavinci.nl>
2023-04-11 12:13:35 +0200

    .

---
+++ module_2/buttons_3.0/script.js
@@ -0,0 +1,29 @@
+const colors = ['red','purple','orange']
+
+function changeCol (button){
+    let buttoncolor = button.style.backgroundColor
+    let colorinaray = colors.indexOf(buttoncolor)
+    if (colorinaray + 1 === colors.length){
+        button.remove()
+    }else{
+        button.style.backgroundColor = colors[colorinaray + 1]
+    }
+    
+
+}
+
+for(let x = 1; x <30 + 1 ; x++){
+    const button = document.createElement('button')
+
+    button.innerText = x
+
+    button.setAttribute('id',x)
+
+    button.setAttribute('onClick','changeCol(this)')
+    document.body.appendChild(button)
+}
+
+      
+
+
+
--- module_2/tank/tank.js
+++ module_2/tank/tank.js
@@ -2,22 +2,66 @@ var image = document.getElementById("image");
 
 document.onkeydown = checkKey;
 image.style.transform = "rotate(90deg)"
+image.style.left = "0px"
+image.style.top = "0px"
+image.style.backgroundPosition = "80px 0px"
 
+positionleft = parseInt(image.style.left)
+positiontop = parseInt(image.style.top)
+backposition = image.style.backgroundPosition.replace(", ", " ").split(" ")
+backpositionx = parseInt(backposition[0])
+
+degdown = "rotate(180deg)"
+degup = "rotate(360deg)"
+degleft = "rotate(270deg)"
+degright = "rotate(90deg)"
+
+function changePosition(direction) {
+    console.log("arrow: " + direction)
+    switch (direction) {
+        case "up":
+            image.style.transform = degup
+            positiontop -= 10
+            backpositionx += 84
+            image.style.top = positiontop.toString() + "px";
+            image.style.backgroundPosition = backpositionx.toString() + "px " + "0px";
+            console.log(image.style.backgroundPosition)
+            return
+        case "down":
+            image.style.transform = degdown
+            positiontop += 10
+            backpositionx += 84
+            image.style.top = positiontop.toString() + "px";
+            image.style.backgroundPosition = backpositionx.toString() + "px " + "0px";
+            return
+        case "left":
+            image.style.transform = degleft
+            positionleft -= 10
+            backpositionx += 84
+            image.style.left = positionleft.toString() + "px";
+            image.style.backgroundPosition = backpositionx.toString() + "px " + "0px";
+            return
+        case "right":
+            image.style.transform = degright
+            positionleft += 10
+            backpositionx += 84
+            image.style.left = positionleft.toString() + "px";
+            image.style.backgroundPosition = backpositionx.toString() + "px " + "0px";
+            return
+    }
+}
 function checkKey(e) {
-	console.log("key nr = " + e.keyCode);
+    console.log("key nr = " + e.keyCode);
     e = e || window.event;
     if(e.keyCode == 32){
-    	console.log("spacebar");
+        console.log("spacebar");
     } else if (e.keyCode == '38') {  // up arrow
-        console.log("Up arrow");
+        changePosition("up")
     } else if (e.keyCode == '40') { // down arrow
-        console.log("down arrow");
+        changePosition("down")
     } else if (e.keyCode == '37') { // left arrow
-    	console.log("left arrow");
+        changePosition("left")
     } else if (e.keyCode == '39') {   // right arrow
-    	console.log("right arrow");
-        while(1==1){
-    	    image.style.backgroundPosition = `164px 0px`; // check goed de rupsband
-        }
+        changePosition("right")
     }
 }
