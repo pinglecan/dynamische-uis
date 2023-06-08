5d4486fabb5d336cb9d5a293aa46110631ac420d
Jelle Fiere <99071834@mydavinci.nl>
2023-06-08 14:44:22 +0200

    .

--- challange/memory/script.js
+++ challange/memory/script.js
@@ -14,17 +14,36 @@ function shuffle(array) {
 
     return array;
 }
+
+function turn_image(img){
+    console.log(img)
+    let name = img.id.replace("_row_", "")
+    console.log(name.replace(/.$/,""))
+    img.src = 'img/' + name.replace(/.$/,"") + '.webp'
+}
+
 //////////////////////////////////////////////////////////////////////////////////////
 
-Images = ["img/aiko.webp"];
+const Images = ["aiko","Audrey","Beli","Celeste","Jessie","Kyanna","kyu","Lola","Momo","Nikki","Tiffany","Venus"];
+
+
+
+for (let x = 1; x < 3; x++){
+    let row = `row_${x}`
+    images = shuffle(Images)
+    
+    for (let i = 0; i < Images.length; i++){
+        let create_image = document.createElement('img');
+        create_image.setAttribute('onclick','turn_image(this)')
+        create_image.src = 'img/background.png' ;
+        create_image.id = String(images[i]) +'_'+ row
+        document.getElementById(row).appendChild(create_image); 
+
+    }
+
+}
 
 
 
 
-
-// for (let i = 0; i < Images; i++){
-create_image = document.createElement('img');
-create_image.src = 'img/cola.jpg';
-document.getElementById('row_1').appendChild(create_image);
-
-
+//img/' + Images[i] + '.webp
